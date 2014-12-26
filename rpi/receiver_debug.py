2989d0500ee3f0aa91b06b4f067b73365cc0fc00
Putu Wiramaswara Widya <wira@ta.wirama.web.id>
2014-12-27 01:47:53 +0700

    add receiver_debug.py

---
+++ rpi/receiver_debug.py
@@ -0,0 +1,23 @@
+import socket
+import json
+import time
+
+port = 5000
+host = '255.255.255.255'
+
+
+# Define UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((host, port))
+
+while True:
+    # Read from Socket
+    dataOutside = s.recvfrom(1024)
+    dataOutside = json.loads(dataOutside)
+    print "Received dataOutside, " + str(dataOutside)
+    time.sleep(5)
+
+
+
